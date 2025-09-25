659cd5b670cf2a42eb12dae8fcc095eee62827c7
pas2rust <pas2000@proton.me>
2025-09-24 21:32:21 -0300

    0.0.4

--- src/components/git.rs
+++ src/components/git.rs
@@ -2,40 +2,70 @@ use super::prelude::*;
 use std::process::Stdio;
 use tokio::process::Command;
 
-pub async fn git_push(
-    token_arg: Option<String>,
-    remote_or_url: &str,
-    branch: &str,
-) -> MyResult<()> {
-    eprintln!("==> Pushing to '{}' branch '{}'", remote_or_url, branch);
-
-    let token = token_arg
-        .or_else(|| std::env::var("GITHUB_TOKEN").ok())
-        .ok_or("GITHUB_TOKEN not set and --token not provided")?;
-
-    let header = format!("http.extraHeader=Authorization: Bearer {}", token);
-
-    let status = Command::new("git")
-        .arg("-c")
-        .arg(header)
-        .arg("push")
-        .arg(remote_or_url)
-        .arg(branch)
-        .stdin(Stdio::inherit())
-        .stdout(Stdio::inherit())
-        .stderr(Stdio::inherit())
+pub async fn setup_git_identity() -> MyResult<()> {
+    let status_name = Command::new("git")
+        .arg("config")
+        .arg("--global")
+        .arg("user.name")
+        .arg("github-actions[bot]")
         .status()
-        .await
-        .map_err(|e| format!("failed to execute `git push`: {}", e))?;
+        .await?;
+    
+    if !status_name.success() {
+        return Err("git config user.name failed".into());
+    }
 
-    if !status.success() {
-        return Err(format!("`git push` exited with code {:?}", status.code()).into());
+    let status_email = Command::new("git")
+        .arg("config")
+        .arg("--global")
+        .arg("user.email")
+        .arg("github-actions[bot]@users.noreply.github.com")
+        .status()
+        .await?;
+    
+    if !status_email.success() {
+        return Err("git config user.email failed".into());
     }
 
     Ok(())
 }
 
-pub async fn git_add<'a>(file_name: &'a str, token: Option<String>) -> MyResult<()> {
+pub async fn git_add_or_update_remote(name: &str, url: &str) -> MyResult<()> {
+    let output = Command::new("git")
+        .arg("remote")
+        .arg("get-url")
+        .arg(name)
+        .output()
+        .await?;
+    
+    if output.status.success() {
+        let status = Command::new("git")
+            .arg("remote")
+            .arg("set-url")
+            .arg(name)
+            .arg(url)
+            .status()
+            .await?;
+        if !status.success() {
+            return Err(format!("git remote set-url {} failed", name).into());
+        }
+    } else {
+        let status = Command::new("git")
+            .arg("remote")
+            .arg("add")
+            .arg(name)
+            .arg(url)
+            .status()
+            .await?;
+        if !status.success() {
+            return Err(format!("git remote add {} failed", name).into());
+        }
+    }
+
+    Ok(())
+}
+
+pub async fn git_add(file_name: &str, token: Option<String>) -> MyResult<()> {
     let token = token
         .or_else(|| std::env::var("GITHUB_TOKEN").ok())
         .ok_or("GITHUB_TOKEN not set")?;
@@ -77,43 +107,25 @@ pub async fn git_commit(message: &str, token: Option<String>) -> MyResult<()> {
     Ok(())
 }
 
-pub async fn git_add_remote(name: &str, url: &str) -> MyResult<()> {
+pub async fn git_push(token: Option<String>, remote: &str, branch: &str) -> MyResult<()> {
+    let token = token
+        .or_else(|| std::env::var("GITHUB_TOKEN").ok())
+        .ok_or("GITHUB_TOKEN not set")?;
+
     let status = Command::new("git")
-        .arg("remote")
-        .arg("add")
-        .arg(name)
-        .arg(url)
+        .arg("-c")
+        .arg(format!("http.extraHeader=Authorization: Bearer {}", token))
+        .arg("push")
+        .arg(remote)
+        .arg(branch)
+        .stdin(Stdio::inherit())
+        .stdout(Stdio::inherit())
+        .stderr(Stdio::inherit())
         .status()
         .await?;
 
     if !status.success() {
-        return Err(format!("git remote add {} failed", name).into());
-    }
-    Ok(())
-}
-
-pub async fn setup_git_identity() -> MyResult<()> {
-    let status_name = Command::new("git")
-        .arg("config")
-        .arg("--global")
-        .arg("user.name")
-        .arg("github-actions[bot]")
-        .status()
-        .await?;
-    
-    if !status_name.success() {
-        return Err("git config user.name failed".into());
-    }
-
-    let status_email = Command::new("git")
-        .arg("config")
-        .arg("--global")
-        .arg("user.email")
-        .arg("github-actions[bot]@users.noreply.github.com")
-        .status()
-        .await?;
-    if !status_email.success() {
-        return Err("git config user.email failed".into());
+        return Err(format!("`git push` exited with code {:?}", status.code()).into());
     }
 
     Ok(())
